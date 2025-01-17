9f7ca6facbede1bbb53e73f0c669b5a2d16e7e16
Ivansyah <ivansyahsantoso@gmail.com>
2025-01-17 19:31:52 +0700

    add views

---
+++ views/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> CRUD - Isi Biodata Diri </title>
+    <link rel="stylesheet" href="/learn-CRUD/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1> Vote </h1>
+        <form action="../php/create.php" method="POST">
+            
+        </form>
+    </div>
+</body>
+</html>
+
