5327eea24292ce731d96614b193ff35997e2cdba
Ivansyah <ivansyahsantoso@gmail.com>
2025-01-17 19:29:51 +0700

    practice create CRUD with OOP

--- config/database.php
+++ config/database.php
@@ -1,15 +1,16 @@
 <?php
 
-function getConnection() {
+function getPDOConnection() {
     $servername = "LAPTOP-A03UJBN1\SQLEXPRESS";
     $username = "";
     $password = "";
     $dbname = "voting";
 
    try {
-        $conn = new PDO("sqlsrv:Server=$servername;Database=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
+        $pdo = new PDO("sqlsrv:Server=$servername;Database=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
     } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
--- models/userModel.php
+++ models/userModel.php
@@ -12,7 +12,34 @@ require_once 'config/database.php';
             return getPDOConnection();
         }
 
-        public function createUser()
+        public function createCustomer($id, $name, $address) {
+            try {
+                $query = "INSERT INTO mahasiswa (id, name, address) VALUES (:id, :name, :address)";
+                $stmt = $this->pdo->prepare($query);
+                $stmt->bindParam(':id', $id);
+                $stmt->bindParam(':name', $name);
+                $stmt->bindParam(':address', $address);
+                $stmt->execute();
+                return ['status' => 'success', 'message' => 'Data berhasil ditambahkan'];
+            } catch (PDOException $e) {
+                return ['status' => 'error', 'message' => $e->getMessage()];
+            }
+        }
+
+        public function readAllUser() {
+            $query = "SELECT * FROM mahasiswa";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function updateUser() {
+
+        }
+
+        public function deleteUser() {
+
+        }
     }
 
 ?>
